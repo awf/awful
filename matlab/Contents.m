9121701a298500d317271467366594e93dec388a
Andrew Fitzgibbon <awf@fitzgibbon.ie>
2013-07-26 15:38:44 +0100

    more doc

--- matlab/Contents.m
+++ matlab/Contents.m
@@ -1,7 +1,33 @@
-% The awf utilities library.
 %
-% au_assert  Assert all(EXPR), print expr if not
-% au_assert_equal  Assert all(EXPR1 == EXPR2), print expr if not
+% A collection of awf utilities
+% 
+% 
+% The au_ prefix is because it's important in matlab's flat namespace
+% that clashes of function names are avoided.
 %
-% au_whist  -  Fast weighted histogram
-% au_sparse -  Fast construction of sparse matrices
+%
+% Symbolic toolbox helpers
+% 
+% AU_COEFF  Coefficients of symbolic polynomial  
+% AU_CCODE  Convert symbolic expression to C with common subexpression elimination  
+% 
+% 
+% Faster alternatives to matlab builtins
+% 
+% AU_SPARSE Create spare matrices more efficiently  
+% AU_WHIST  Weighted histogram  
+% 
+% 
+% Printing and testing
+% 
+% AU_PRMAT   Compact matrix printing  
+% AU_TEST*   Utilities for writing unit tests  
+% AU_ASSERT* Easier assertions  
+% 
+% 
+% Optimization/Numerics
+% 
+% AU_LEVMARQ     Simple Levenberg-Marquardt  
+% AU_RANSAC      Simple RANSAC  
+% AU_ROSENBROCK  Rosenbrock test function  
+% AU_LOGSUMEXP   Efficient and save log(sum(exp(V)))  

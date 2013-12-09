efcfb498d1b891e15ac41c79a176a48263754392
awf <awf@fitzgibbon.ie>
2013-12-09 17:26:47 +0000

    add symbolic differentiator, somewhat confusingly called autodiff

---
+++ matlab/au_autodiff_generate.m
@@ -0,0 +1,105 @@
+function str = au_autodiff_generate(function_handle, example_arguments, example_data, filename)
+% AU_AUTODIFF_GENERATE  Generate code for function and derivatives
+%      To generate C code for a function and its jacobian, use
+%       STR = AU_AUTODIFF_GENERATE(FUNCTION_HANDLE, EXAMPLE_ARG, EXAMPLE_DATA)
+%      or 
+%       AU_AUTODIFF_GENERATE(FUNCTION_HANDLE, EXAMPLE_ARG, EXAMPLE_DATA, FILE)
+%      The first form returns the core code in a string, the second wraps
+%      it in a MEX wrapper, and stores it in file FILE.
+%      The EXAMPLE_ARG and EXAMPLE_DATA are used to determine the size, and
+%      to perform finite-difference checks just in case...
+%
+%      FUNCTION_HANDLE must take a column vector and return a column vector.  
+%      It can also take an optional argument DATA.
+
+% awf, dec13
+
+if nargin == 0
+    f = @(x,data) [sin(x(3)/norm(x(1:2))); 4*cos((6*x(1)-6*x(3))+x(2)^2)];
+    p = [1 2 3]';
+    f(p)
+    
+    au_autodiff_generate(f, p, [], 'c:/tmp/au_autodiff_generate_test.cpp')
+    % type c:\tmp\au_autodiff_generate_test.cpp
+    fprintf('mexing... c:\\tmp\\au_autodiff_generate_test.cpp\n')
+    mex -I. c:/tmp/au_autodiff_generate_test.cpp
+    
+    out = au_autodiff_generate_test([p p p], zeros(0,3));
+    out_val = out(1:2,1)
+    out_jac = reshape(out(3:end,1), 2, 3)
+    
+    return
+end
+
+if nargin < 4
+    filename = [];
+end
+
+au_assert size(example_arguments,2)==1
+out = function_handle(example_arguments, example_data);
+au_assert size(out,2)==1
+
+m = size(example_arguments,1);
+n = size(out,1);
+md = size(example_data,1);
+
+in = sym('x', [m 1]);
+sym(in, 'real');
+data = sym('data', [md 1]);
+sym(data, 'real');
+
+fprintf('au_autodiff_generate: making code for f:R^%d->R^%d\n', m, n);
+out_val = function_handle(in, data);
+fprintf('au_autodiff_generate: computing jacobian\n');
+out_jac = jacobian(out_val, in);
+
+out = [out_val; out_jac(:)];
+% awf xxfixme: handle symbolic sparsity find(~(out_all == 0))
+
+s = '';
+for k=1:m
+    s= [s sprintf('    double x%d = in_ptr[%d];\n', k, k-1)];
+end
+for k=1:md
+    s= [s sprintf('    double data%d = data_ptr[%d];\n', k, k-1)];
+end
+fprintf('au_autodiff_generate: computing ccode\n');
+str = [s au_ccode(out)];
+str = strrep(str, '[0 * out_rows + ', '[');
+
+if isempty(filename)
+    return
+end
+
+%% If there is a filename, make it a mexFunction
+fprintf('au_autodiff_generate: writing file\n');
+
+% File descriptor or name?
+if ischar(filename)
+    fd = fopen(filename, 'w');
+else
+    fd = filename;
+end
+
+body = '  /* inner loop */';
+body = sprintf('%s\n%s', body, str);
+
+% Get Template Text
+tfd = fopen('au_autodiff_generate_template.cpp', 'r');
+template = fread(tfd, inf, 'char');
+fclose(tfd);
+
+varname = inputname(1);
+if isempty(varname)
+    varname = '[Anonymous expression]';
+end
+
+template = char(template');
+template = strrep(template, '@VarName', varname);
+template = strrep(template, '@Body', body);
+template = strrep(template, '@OutRows', num2str(size(out,1)));
+
+fprintf(fd, '%s', template);
+if ischar(filename)
+    fclose(fd);
+end

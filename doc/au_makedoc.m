a539fd4eb51ecb0571cc1d99d076db4d815d4513
awf <awf@fitzgibbon.ie>
2014-03-20 10:20:39 +0000

    Print filenames

--- doc/au_makedoc.m
+++ doc/au_makedoc.m
@@ -5,6 +5,7 @@ matlab_files = what(au_root_dir);
 for mfile = (matlab_files.m)'
     mfile = mfile{1};
     if ~strcmp(mfile, 'Contents.m')
+        fprintf(2,'%s\n', mfile);
         help(mfile)
     end
 end

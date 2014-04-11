781f800649d5bb67b765eb74bcc4c6a74044a859
awf <awf@fitzgibbon.ie>
2014-04-11 18:14:49 +0100

    Run all tests

---
+++ matlab/au_run_tests.m
@@ -0,0 +1,17 @@
+
+% Test the testing code
+au_test_test
+
+% Test the rest
+au_bsx_test
+au_mfilename_test
+au_prmat_test
+au_sparse_test
+au_whist_test
+
+au_ccode_test
+
+au_levmarq_test
+
+au_mex_test
+

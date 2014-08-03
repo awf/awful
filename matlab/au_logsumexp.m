9a2eaf86e0e57c9c7bc09589d68429dae6ecdbda
awf <awf@fitzgibbon.ie>
2014-08-04 00:50:52 +0100

    Fixed Jacobian

--- matlab/au_logsumexp.m
+++ matlab/au_logsumexp.m
@@ -2,16 +2,19 @@ function [l, Jacobian] = au_logsumexp(M)
 % AU_LOGSUMEXP  Compute log(sum(exp(M))) stably
 %                   au_logsumexp(M) = log(sum(exp(M))) 
 %                 but avoids under/overflow.
+%               [L, Jacobian] = au_logsumexp(M) returns the Jacobian
+%               Notice that although sum operates along columns,
+%               L is returned as a column vector so that the Jacobian 
+%               makes sense.
 
 % awf, may13
 
 if nargin == 0
   %% test
   Ms = {
+    -rand(7,1)
     -rand(7,3)
     -rand(1,7)
-    -rand(7,1)
-    randn(7,3,4)
     [0 0 0 -5 -10 -15 -20 -10 -5]-1000
     [0 0 0 -5 -10 -15 -20 -10 -5]+900
     };
@@ -19,19 +22,41 @@ if nargin == 0
     M = Ms{k};
     fprintf('test %d size', k); fprintf(' %d', size(M)); fprintf('\n');
     lseM = log(sum(exp(M)));
-    au_lseM = au_logsumexp(M);
+    [au_lseM, J] = au_logsumexp(M);
     q = @(x) squeeze(x);
-    au_prmat(q(lseM), q(au_lseM));
+    %au_prmat(q(lseM), q(au_lseM));
+    au_check_derivatives(@(x) au_logsumexp(reshape(x, size(M)))', M(:), J);
   end
+  
   return
+end
+
+if isa(M,'sym')
+    l = log(sum(exp(M)));
+    return
 end
 
 %% Main body
 sz = size(M);
 if prod(sz) == max(sz)
   A = max(M);
-  l = log(sum(exp(M - A))) + A;
+  ema = exp(M-A);
+  sema = sum(ema);
+  l = log(sema) + A;
+  if nargout > 1
+      Jacobian = ema(:)'./sema;
+  end
 else
   A = max(M);
-  l = bsxfun(@plus, log(sum(exp(bsxfun(@minus, M, A)))), A);
+  ema = exp(bsxfun(@minus, M, A));
+  sema = sum(ema);
+  l = bsxfun(@plus, log(sema), A)';
+  if nargout > 1
+      [r,c] = size(M);
+      JacobEntries = bsxfun(@rdivide, ema, sema);
+      Jacobian = sparse(...
+          repmat(1:c, r, 1), ...
+          reshape(1:r*c, r, c), ...
+          JacobEntries);
+  end
 end

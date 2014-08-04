b37823605a0cb0bc31dc6c086b021c8dc714b377
awf <awf@fitzgibbon.ie>
2014-08-04 22:05:08 +0100

    more doc, gitignore

--- matlab/au_mex.h
+++ matlab/au_mex.h
@@ -9,6 +9,24 @@
  * an mxArray.  To write a fully error-checked mex file which adds two 
  * double arrays, write this:
  *
+ *     #include "au_mex.h"
+ *    
+ *     // Declare mlx_function (C++ version of mexFunction)
+ *     void mlx_function(mlx_inputs& in, mlx_outputs& out)
+ *     {
+ *        mlx_array<mlx_double> A(in[0]); // Get input 0
+ *        mlx_array<mlx_double> B(in[1]); // Get input 1
+ *       
+ *        mlx_make_array<double> sum(A.size); // Make output array
+ *       
+ *        mlx_assert(A.size == B.size); // Check sizes are equal
+ *       
+ *        // Perform the operation
+ *        for(mwSize i = 0; i < A.numel(); ++i)
+ *          sum[i] = A[i] + B[i];
+ *       
+ *        out[0] = sum; // Assign to output
+ *     }
  * 
  * */
 

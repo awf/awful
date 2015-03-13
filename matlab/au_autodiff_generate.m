55902b8ce4f1e34cf7f5fbf327ee484a3e72edf7
awf <awf@microsoft.com>
2015-03-13 16:35:32 -0700

    Use au_opts

--- matlab/au_autodiff_generate.m
+++ matlab/au_autodiff_generate.m
@@ -1,4 +1,4 @@
-function str = au_autodiff_generate(function_handle, example_arguments, example_data, filename, DO_CSE)
+function str = au_autodiff_generate(function_handle, example_arguments, example_data, filename, varargin)
 % AU_AUTODIFF_GENERATE  Generate code for function and derivatives
 %      To generate C code for a function and its jacobian, use
 %       STR = AU_AUTODIFF_GENERATE(FUNCTION_HANDLE, EXAMPLE_ARG, EXAMPLE_DATA)
@@ -23,11 +23,12 @@ if nargin == 0
     au_autodiff_generate(f, p, [], 'c:/tmp/au_autodiff_generate_test_mex.cpp');
  
     % Check it exists and is callable
-    do_jacobian = true;
-    out = au_autodiff_generate_test_mex([p p p], zeros(0,3), do_jacobian);
-    out_val = out(1,:)
-    out_jac = out(2:end,:)
-
+    for do_jacobian = [false true]
+      out = au_autodiff_generate_test_mex([p p p], zeros(0,3), do_jacobian);
+      out_val = out(1,:)
+      out_jac = out(2:end,:)
+    end
+    
     au_test_should_fail o=au_autodiff_generate_test_mex(rand(4,1),zeros(0,1),false)
     au_test_should_fail o=au_autodiff_generate_test_mex(rand(3,1),zeros(2,1),false)
 
@@ -38,9 +39,7 @@ if nargin < 4
     filename = [];
 end
 
-if nargin < 5
-  DO_CSE = 1;
-end
+opts = au_opts('CSE=1;JACOBIAN=1;SIMPLIFY_TMAX=0', varargin{:});
 
 %% Determine sizes etc
 au_assert size(example_arguments,2)==1
@@ -61,19 +60,23 @@ tic
 fprintf('au_autodiff_generate: making code for f:R^%d->R^%d ', m, n);
 out_val = function_handle(in, data);
 fprintf(' [%.1fsec]\n', toc);
-fprintf('au_autodiff_generate: computing jacobian ... ');
-times=[];
-for k=numel(in):-1:1
+if opts.JACOBIAN
+  fprintf('au_autodiff_generate: computing jacobian ... ');
+  times=[];
+  for k=numel(in):-1:1
+    tic
+    out_jac(:,k) = diff(out_val, in(k));
+    times(k) = toc;
+    fprintf(' %d [%.1fsec]', k, times(k));
+  end
+  fprintf(' %.1fsec\n', sum(times));
+  fprintf('au_autodiff_generate: transpose ... ');
   tic
-  out_jac(:,k) = diff(out_val, in(k));
-  times(k) = toc;
-  fprintf(' %d [%.1fsec]', k, times(k));
+  out = [out_val out_jac].';
+  fprintf(' %.1fsec\n', toc);
+else
+  out = out_val;
 end
-fprintf(' %.1fsec\n', sum(times));
-fprintf('au_autodiff_generate: transpose ... ');
-tic
-out = [out_val out_jac].';
-fprintf(' %.1fsec\n', toc);
 
 % awf xxfixme: handle symbolic sparsity find(~(out_all == 0))
 
@@ -86,15 +89,14 @@ for k=1:md
     decls= [decls sprintf('    double data%d = data_ptr[%d];\n', k, k-1)];
 end
 fprintf('au_autodiff_generate: computing ccode for do_jacobian=0\n');
-BodyNoJac = [decls au_ccode(out_val, [], DO_CSE)];
+BodyNoJac = [decls au_ccode(out_val, [], opts.CSE, opts.SIMPLIFY_TMAX)];
 BodyNoJac = strrep(BodyNoJac, '[0 * out_rows + ', '[');
 str = BodyNoJac;
 
-DO_JAC = 1;
-if DO_JAC
+if opts.JACOBIAN
   tic
   fprintf('au_autodiff_generate: computing ccode for do_jacobian=1\n');
-  BodyJacobian = [decls au_ccode(out, [], DO_CSE)];
+  BodyJacobian = [decls au_ccode(out, [], opts.CSE, opts.SIMPLIFY_TMAX)];
   BodyJacobian = strrep(BodyJacobian, '[0 * out_rows + ', '[');
   codegen_time = toc;
   str = BodyJacobian;
@@ -117,7 +119,7 @@ end
 
 body0 = '  /* inner loop do_jac=0 */';
 body0 = sprintf('%s\n%s', body0, BodyNoJac);
-if DO_JAC
+if opts.JACOBIAN
   body1 = '  /* inner loop do_jac=1 */';
   body1 = sprintf('%s\n%s', body1, BodyJacobian);
 else
@@ -162,12 +164,12 @@ if ischar(filename)
         
     fprintf('au_autodiff_generate: testing... [%s]\n', fn)
     val_f = function_handle(example_arguments, example_data);
-    val_mex = feval(fn, example_arguments(:), example_data(:), DO_JAC);
+    val_mex = feval(fn, example_arguments(:), example_data(:), opts.JACOBIAN~=0);
     
     val_f = val_f(:)';
     au_test_equal val_f val_mex(1,:) 1e-8
 
-    if DO_JAC
+    if opts.JACOBIAN
       J = val_mex(2:end,:)';
       f_dc = @(x) feval(fn, x, example_data(:), false)';
       timeout = max(2, codegen_time/10);

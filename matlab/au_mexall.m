bcb294f7225f3af991cc4b8a9411267002a2ca75
awf <awf@microsoft.com>
2015-07-06 16:53:04 +0100

    deep vectorization

--- matlab/au_mexall.m
+++ matlab/au_mexall.m
@@ -6,6 +6,8 @@ try
     cd(newdir)
     domex -largeArrayDims au_sparse.cxx
     domex au_whist.cxx
+    domex au_deep_vectorize_mex.cxx
+    domex au_deep_unvectorize_mex.cxx
 catch e
     cd(d)
     rethrow(e)

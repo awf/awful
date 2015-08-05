691d70c6f674998b2ab77612cf1847c1367a621a
awf <awf@microsoft.com>
2015-08-05 09:31:07 +0100

    new figs

--- examples/robust_bundle_revisited/rba_figures.m
+++ examples/robust_bundle_revisited/rba_figures.m
@@ -6,14 +6,17 @@ ws = linspace(0,2,3000);
 f = rba_psi(tts, wws);
 
 clf
+set(0, 'defaulttextfontsize', 22)
 subplot(212)
 plot(ts, rba_psi(ts), 'k')
 hold on
 plot(ts, min(f), 'ro', 'markersize', 2);
+xlabel x
+ylabel \psi(x)
 
 axis([-3 3 0 1.1])
 
-%%
+%
 subplot(211)
 wmin = 0.01;
 ts = linspace(-3, 3, 200);
@@ -37,8 +40,15 @@ n=sqrt(px.^2+py.^2);
 scale = 1./n;
 p=quiver(tts, wws, -px.*scale,-py.*scale, .5, 'k');
 set(p, 'color', [1 1 1]*.7)
-xlabel r
+xlabel x
 ylabel w
 axis([-3 3 0 2.5])
+legend('contours of \phi', 'argmin_w \phi(w,x)')
 
-legend('contours of \kappa', 'argmin_w \kappa(w,t)')
+
+%%
+set(gcf, 'paperunits', 'centi', 'paperposition', [0 0 10 11])
+print -depsc rba_figures.eps
+!unix2dos rba_figures.eps
+!epstopdf rba_figures.eps 
+!rba_figures.pdf

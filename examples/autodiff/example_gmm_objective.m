3fc551336487f6622afd44d1394b0311eb0adf68
awf <awf@microsoft.com>
2015-08-05 09:31:01 +0100

    new autogen

--- examples/autodiff/example_gmm_objective.m
+++ examples/autodiff/example_gmm_objective.m
@@ -1,13 +1,13 @@
-function err = example_gmm_objective(params, data)
+function err = example_gmm_objective(params, data, get_lse_offset)
 % EXAMPLE_GMM_OBJECTIVE  Evaluate GMM negative log likelihood for one point
 %         First argument PARAMS stores GMM
-%             params.log_alphas  
+%             params.log_alphas
 %                vector of logs of mixture weights (unnormalized), so
 %                weights = exp(log_alphas)/sum(exp(log_alphas))
 %             params.means
 %                Cell array of component means (each dimension dx1)
-%             params.inv_cov_factors 
-%                Cell array of d*(d+1)/2 vectors, parametrizing 
+%             params.inv_cov_factors
+%                Cell array of d*(d+1)/2 vectors, parametrizing
 %                lower triangular square roots of inverse covariances
 %                log of diagonal is first d params
 %      To generate params given covariance C:
@@ -27,67 +27,118 @@ function err = example_gmm_objective(params, data)
 % =log(sum_k exp(alphas[k])/sum(exp(alphas)) * exp(log(det(Ls[k])) * exp(-0.5*sumsq(mahal_k)))
 % =log(1/sum(exp(alphas)) * sum_k { exp(alphas[k]) * exp(log(det(Ls[k])) * exp(-0.5*sumsq(mahal_k)))
 % =log(1/sum(exp(alphas)) * sum_k exp(alphas[k]) + log(det(Ls[k])) - 0.5*sumsq(mahal_k)))
-% =-log(sum(exp(alphas)) + 
+% =-log(sum(exp(alphas)) +
 %       log(sum_k exp(alphas[k] + log(det(Ls[k])) - 0.5*sumsq(mahal_k)))
-% =-log(sum(exp(alphas)) + 
+% =-log(sum(exp(alphas)) +
 %       log(sum_k exp(alphas[k] + log(prod(diag(Ls[k]))) - 0.5*sumsq(mahal_k)))
-% =-log(sum(exp(alphas)) + 
+% =-log(sum(exp(alphas)) +
 %       log(sum_k exp(alphas[k] + log(prod(exp(diag(Ls0[k])))) - 0.5*sumsq(mahal_k)))
-% =-log(sum(exp(alphas)) + 
+% =-log(sum(exp(alphas)) +
 %       log(sum_k exp(alphas[k] + sum(diag(Ls0[k])) - 0.5*sumsq(mahal_k)))
 
 
 
 if nargin == 0
-    %%
-    
-    % Make a small random GMM
-    d = 2;
-    K = 4;
-    params.log_alphas = randn(K,1);
-    params.means = au_map(@(i) rand(d,1), cell(K,1));
-    params.inv_cov_factors = au_map(@(i) randn(d*(d+1)/2,1), cell(K,1));
-
-    % Flatten the parameters into a vector
-    x = au_deep_vectorize(params);
-    
-    % And make an "unflattening" function
-    unvec = @(x) au_deep_unvectorize(params, x);
-    
-    % Make some random data
-    data = [
-        randn(d,1);
-        1.0];
-    
-    % Test call of the function
-    f = @(x,data) example_gmm_objective(unvec(x), data);
-    f(x, data)
-    
-    au_autodiff_generate(f, x, data, 'example_gmm_objective_mex.cxx')
-    return
+  %%
+  
+  % Make a small random GMM
+  d = 10;
+  K = 25;
+  params.log_alphas = randn(K,1);
+  params.means = au_map(@(i) rand(d,1), cell(K,1));
+  params.inv_cov_factors = au_map(@(i) randn(d*(d+1)/2,1), cell(K,1));
+  
+  % Flatten the parameters into a vector
+  x = au_deep_vectorize(params);
+  
+  % And make an "unflattening" function
+  unvec = @(x) au_deep_unvectorize(params, x);
+  
+  % Make some random data
+  data = [
+    randn(d,1); % point x
+    inf   % logsumexp offset -- to be filled in
+    2.0   % Wishart gamma^2/2
+    1     % Wishart excess dof
+    ];
+  % Get max LSE for LSE offset.  In practice would need another
+  % mexed variant of the function to extract this.
+  data(d+1) = example_gmm_objective(unvec(x), data, true);
+  
+  % Test call of the function
+  f = @(x,data) example_gmm_objective(unvec(x), data, false);
+  f(x, data)
+  
+  mexname = sprintf('example_gmm_objective_mex_d%d_K%d', d, K);
+  au_autodiff_generate(f, x, data, [mexname '.cxx']);
+  
+  %%
+  mexname = 'example_gmm_objective_mex';
+  n=2500;
+  ts = [0 0];
+  for dojac = 0:1
+    fprintf('running trial: dojac = %d ...', dojac);
+    Drep = repmat(data, 1, n);
+    tic;
+    Ntrials = 100;
+    for k=1:Ntrials;
+      [~] = feval(mexname, repmat(x, 1, n), Drep, dojac==1);
+    end
+    t = toc/Ntrials;
+    fprintf('running trial: t = %g\n', t);
+    ts(dojac+1) = t;
+  end
+  %
+  fprintf('example_gmm(%d,%d)->%d: %d: t0 = %g, t1 = %g, ratio = %.2f\n', ...
+    d, K, numel(x), n, ts(1), ts(2), ts(2)/ts(1));
+  return
 end
 
-d = size(data,1)-1;
+d = size(data,1)-3;
+x = data(1:d);
+logsumexp_offset = data(d+1);
+wishart_gammasquaredover2 = data(d+2);
+wishart_m = data(d+3);
+
+
 K = size(params.log_alphas,1);
 log_alphas = params.log_alphas;
 means = [params.means{:}];
 
 lower_triangle_indices = tril(ones(d,d), -1) ~= 0;
 
-x = data(1:d);
-logsumexp_offset = data(d+1);
 
 lse = zeros(K,1, 'like', log_alphas);
 for k=1:K
-    % Unpack L parameters into d*d matrix.
-    Lparams = params.inv_cov_factors{k};
-    % Set L's diagonal
-    logLdiag = Lparams(1:d);
-    L = diag(exp(logLdiag));
-    % And set lower triangle
-    L(lower_triangle_indices) = Lparams(d+1:end);
-
-    mahal = L*(means(:,k) - x);
-    lse(k) = log_alphas(k) + sum(logLdiag) - 0.5*(mahal'*mahal);
+  % Unpack L parameters into d*d matrix.
+  Lparams = params.inv_cov_factors{k};
+  % Set L's diagonal
+  logLdiag = Lparams(1:d);
+  L = diag(exp(logLdiag));
+  % And set lower triangle
+  L(lower_triangle_indices) = Lparams(d+1:end);
+  
+  mahal = L*(means(:,k) - x);
+  lse(k) = log_alphas(k) + sum(logLdiag) - 0.5*(mahal'*mahal);
 end
+
+% If user just wants to know the LSE offset, compute and return now
+if get_lse_offset
+  err = max(lse);
+  return
+end
+
+% Pass LSE through logsumexp
 err = log(sum(exp(lse-logsumexp_offset)))+logsumexp_offset - au_logsumexp(log_alphas);
+
+% Wishart
+for k=1:K
+  % Unpack L parameters into d*d matrix.
+  Lparams = params.inv_cov_factors{k};
+  % Set L's diagonal
+  Ldiag = exp(Lparams(1:d));
+  Lltri = Lparams(d+1:end);
+  err = err + ...
+    wishart_gammasquaredover2 * (sum(Ldiag.^2) + sum(Lltri.^2)) - ...
+    wishart_m * sum(Ldiag);
+end

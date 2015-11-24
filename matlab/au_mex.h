2049a3863456ff3bf4aa24afe39d7c1e8cefef2a
awf <awf@microsoft.com>
2015-11-24 11:39:58 +0000

    Consistent bounds checking

--- matlab/au_mex.h
+++ matlab/au_mex.h
@@ -250,7 +250,9 @@ struct mlx_array {
 
   // 0-based []
   T const& operator[](mwIndex ind) const {
+#ifndef AU_MEX_UNCHECKED
     mlx_assert(ind >= 0 && ind < numel_);
+#endif
     return data[ind];
   }
 

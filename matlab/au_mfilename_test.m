b3ea66778ef857e70508035836e1f612eb955709
Andrew Fitzgibbon <awf@microsoft.com>
2017-04-03 16:29:13 +0100

    Make work better from base workspace

--- matlab/au_mfilename_test.m
+++ matlab/au_mfilename_test.m
@@ -1,4 +1,8 @@
-function au_mfilename_test
+function au_mfilename_test(caller_name)
+
+if nargin < 1
+  caller_name = 'base workspace';
+end
 
 au_test_regexp(au_mfilename, 'au_mfilename_test')
-au_test_regexp(au_mfilename(-1), 'base workspace')
+au_test_regexp(au_mfilename(-1), caller_name)
